b2f15aac26d1ecac7c57b3acca563fcf0e687fdf
bbggkkk <bbggkkk@naver.com>
2022-04-09 20:46:43 +0900

    연결 설정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,10 +1,34 @@
-const ws = new WebSocket(`ws://${window.location.host}`);
+const ws = new WebSocket(`wss://${window.location.host}`);
+
+navigator.geolocation.getCurrentPosition((position) => {
+    const la = (position.coords.latitude).toFixed(1);
+    const lo = (position.coords.longitude).toFixed(1);
+
+    const option = {
+        type      : 'GET_POSITION',
+        name      : la+'/'+lo,
+        latitude  : la,
+        longitude : lo
+    }
+    ws.send(JSON.stringify(option));
+});
 
 ws.addEventListener('open', (socket) => {
     console.log('Connection ✅');
 });
-ws.addEventListener('message', (socket) => {
-    console.log(socket.data);
+ws.addEventListener('message', ({data}) => {
+    const rt  = JSON.parse(data);
+    console.log(rt);
+    const key = rt.key;
+    document.querySelector('#id').innerText = key;
+    document.querySelector('#msg-wrap').innerHTML = '';
+    rt.list.forEach(item => {
+        const ele   = document.createElement('li');
+        const p     = document.createElement('p');
+        p.innerText = item.name;
+        ele.append(p);
+        document.querySelector('#msg-wrap').append(ele);
+    })
 });
 ws.addEventListener('close', (socket) => {
     console.log('Connection ❌');
@@ -13,7 +37,14 @@ ws.addEventListener('close', (socket) => {
 const btn = document.querySelector('#send-btn');
 const inp = document.querySelector('#input');
 
-btn.addEventListener('click', (e) => {
+const handleSendMsg = () => {
     ws.send(inp.value);
     inp.value = '';
+}
+
+inp.addEventListener('keydown', (e) => {
+    if(e.keyCode === 13){
+        handleSendMsg();
+    }
 });
+btn.addEventListener('click', handleSendMsg);
--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import express      from 'express';
-import http         from 'http';
+import https         from 'https';
 import WebSocket    from 'ws';
 
+import fs           from 'fs';
+
 const app = express();
+
+const option = {
+    key : fs.readFileSync(__dirname+'/cert/localhost-key.pem'),
+    cert: fs.readFileSync(__dirname+'/cert/localhost.pem')
+}
 
 app.set('view engine', 'pug');
 app.set('views', __dirname+'/views');
@@ -11,18 +18,54 @@ app.use('/public', express.static(__dirname+'/public'));
 app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
-const handleListening = () => console.log('Listening on http://localhost:3000');
+const handleListening = () => console.log('Listening on https://localhost:3000');
 
-const server = http.createServer(app);
-const wss    = new WebSocket.Server({server});
+const server  = https.createServer(option, app);
+const wss     = new WebSocket.Server({server});
+let   sockets = {};
 
-wss.on('connection', (socket) => {
+wss.on('connection', (socket, req) => {
+    const header = req.headers;
+    socket.agent    = header['user-agent'];
+    socket.uid      = header['sec-websocket-key'];
+    socket.name     = (Number(''+(new Date().getTime())+'001').toString(36).substring(7,10))+(socket.uid.substring(0,4));
+
     socket.on('close', () => {
-        console.log('disconnected from client');
+        const position = socket.position;
+        if(position){
+            delete sockets[position][socket.uid];
+            wss.clients.forEach(item => item.send(JSON.stringify(
+                {
+                    list : Object.values(sockets[position]),
+                    key  : item.name
+                }
+            )));
+            (Object.values(sockets[position]).length === 0) && (delete sockets[position]);
+        }
     });
     socket.on('message', (message) => {
-        const msg = message.toString();
-        socket.send('server recieved :: '+msg)
+        const data  = JSON.parse(message);
+
+        switch (data.type) {
+            case 'GET_POSITION' :
+                const msg       = data.name;
+                socket.position = msg;
+                sockets[msg]    = sockets[msg] ? sockets[msg] : {};
+                sockets[msg][socket.uid] = {
+                    position : socket.position,
+                    name     : socket.name,
+                    uid      : socket.uid,
+                };
+                wss.clients.forEach(item => {
+                    const rt = JSON.parse(JSON.stringify(sockets[msg]));
+                    delete rt[item.uid];
+                    item.send(JSON.stringify({
+                        list : Object.values(rt),
+                        key  : item.name
+                    }));
+                });
+                break;
+        }
     });
 });
 
